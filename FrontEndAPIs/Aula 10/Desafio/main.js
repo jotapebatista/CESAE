c8663ef25d4464188503bb201be1bae9c6bf4c4a
João Batista <joao-oliveirabatista@hotmail.com>
2022-10-20 00:19:00 +0100

    beutified html image presentation

--- FrontEndAPIs/Aula 10/Desafio/main.js
+++ FrontEndAPIs/Aula 10/Desafio/main.js
@@ -13,10 +13,10 @@ async function getData() {
 async function getImageUrl() {
     let imageUrl = await getData()
     //console.log(imageUrl)
-    if  (imageUrl.status  !== 'success'){
+    if (imageUrl.status !== 'success') {
         alert("Oops! Error downloading image!")
     }
-    else{
+    else {
         drawImage(imageUrl.message)
     }
 }
@@ -26,19 +26,45 @@ function drawImage(imgResponse) {
     //console.log(imgList)
 }
 
-let buttonsonPage = document.getElementsByTagName('button')
-buttonsonPage[0].onclick = (ev) =>{
-    ev.preventDefault()
-    getImageUrl()
-}
+let buttonsClick = document.getElementsByTagName('button')
+// buttonsClick[0].onclick = (ev) =>{
+//     ev.preventDefault()
+//     getImageUrl()
+// }
 
-buttonsonPage[1].onclick = (ev) =>{
+buttonsClick[1].onclick = (ev) => {
     ev.preventDefault()
     arrayLenght = Object.keys(imgList).length
     previousImage = arrayLenght - 2
     console.log(arrayLenght)
-    console.log (previousImage)
+    console.log(previousImage)
     console.log(imgList[previousImage].message)
     drawImage(imgList[previousImage].message)
 }
 
+
+function getMultipleFetchCalls() {
+    Promise.all([
+        fetch('https://dog.ceo/api/breeds/image/random'),
+        fetch('https://dog.ceo/api/breeds/image/random'),
+        fetch('https://dog.ceo/api/breeds/image/random')
+    ]).then(function (responses) {
+        // Get a JSON object from each of the responses
+        return Promise.all(responses.map((response) => {
+            return response.json();
+        }));
+    }).then((data) => {
+        imgList.push(data)
+        console.log(imgList[0][0].message)
+        //document.querySelector(".image-container").innerHTML = `<img src="${imgList[0][0].message}" class=' rounded mx-auto d-block' style='max-width: 40%; height:40%;'">`
+        buttonsClick[0].onclick = (ev) => {
+            imgList.forEach(element => {
+                document.querySelector(".image-container").innerHTML = `<img src="${element}" class=' rounded mx-auto d-block' style='max-width: 40%; height:40%;'">`
+                console.log(imgList)
+            });
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+getMultipleFetchCalls()
