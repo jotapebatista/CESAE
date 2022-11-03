bce9c7ed124cdabbcc0187eb99e8378982f44688
João Batista <jb@brightstuff.pt>
2022-11-03 17:35:12 +0000

    finished exercise

--- FrontEndAPIs/Projetos/UserManagement/main.js
+++ FrontEndAPIs/Projetos/UserManagement/main.js
@@ -32,36 +32,37 @@ async function getUserData() {
         const avatarElement = li.querySelectorAll('img')
         const nameElement = li.querySelectorAll('h3')
         const emailElement = li.querySelectorAll('div')
+        const gridElement = li.querySelectorAll('.grid-item')
 
-        // const newEmail = li.children[0].children[2]
-        // newEmail.innerHTML = personList[0].email
-
-      console.log(emailElement)
-
+        //User Picture
         avatarElement.forEach((img, index) => {
             img.src = personList[index].picture
         })
 
+        //User name
         nameElement.forEach((h3, index) => {
             h3.innerHTML = personList[index].fullname
         })
 
-        // emailElement.forEach((h3, index) => {
-        //     h3.innerHTML = personList[index].fullname
-        // })
+        //User Email
+        for (let i = 0; i < gridElement.length; i++) {
+            //console.log(i) // 0 -> 5 means that found 5 cards in the HTML body
+            //console.log(gridElement[i].children[2].children[0].textContent)
+            gridElement[i].children[2].children[0].textContent = `${personList[i].email}`
 
+        }
 
-        // if (emailElement.textContent.includes("Email: ")) {
-        //     emailElement.forEach((p, index) => {
-        //         p.innerHTML = personList[index].email
-        //     })
-        // }
+        //User Phone Number
+        for (let i = 0; i < gridElement.length; i++) {
+            gridElement[i].children[3].children[0].textContent = `${personList[i].phone}`
+        }
 
-        let elementHtml = [].map.call(document.querySelectorAll('div'), function(el) {
-            return el.innerHTML;
-        }).join();
-
-        console.log(elementHtml)
+        //User DOB
+        for (let i = 0; i < gridElement.length; i++) {
+            let date = personList[i].dob 
+            let dateStr = date.substr(0, 10)
+            gridElement[i].children[4].children[0].textContent = `${dateStr}`
+        }
 
 
 
