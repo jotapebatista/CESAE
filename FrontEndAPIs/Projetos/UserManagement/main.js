7046ab1f821285237a01fdcc42044d9c41146c8f
João Oliveira <joao-oliveirabatista@hotmail.com>
2022-11-04 08:36:59 +0000

    nem solution for usermanagement

--- FrontEndAPIs/Projetos/UserManagement/main.js
+++ FrontEndAPIs/Projetos/UserManagement/main.js
@@ -1,26 +1,130 @@
 let personList = []
 
 class User {
-    constructor({ name, email, phone, dob, picture }) {
+    constructor({ name, email, phone, dob, age, picture, nat, location }) {
         this.fullname = `${name.title} ${name.first} ${name.last}`
         this.email = email
         this.phone = phone
         this.dob = dob.date
+        this.age = dob.age
+        this.nat = nat
+        this.location = location
         this.picture = picture.large
     }
-
-    getuserData() {
-        return this.fullname
-    }
-
 }
 
+async function requestApi() {
+    try {
+        userQty = prompt('Users:')
+        const response = await fetch(`https://randomuser.me/api/?results=${userQty}`)
+        //const response = await fetch('https://randomuser.me/api/')
+
+        const data = await response.json()
+        //console.log(data)
+        data.results.forEach(function (user) {
+            const person = new User(user)
+            personList.push(person)
+        })
+
+        addElement(personList)
+    } catch (error) {
+        console.error(error) // from creation or business logic
+    }
+}
+
+
+function addElement(personObj) {
+    // create Elements
+    //Main card div
+
+    let dateStr = (personObj[0].dob).toString()
+    let newDate = new Date(dateStr)
+    newParsedDate = newDate.getDate() + "/" + (newDate.getMonth() + 1) + "/" + (newDate.getFullYear())
+
+    //FIXME: delete this
+
+    for (let i = 0; i < personObj.length; i++) {
+        //Card Structure & Styles
+        const mainUl = document.createElement("ul")
+        mainUl.classList.add("users-grid")    
+        const mainDiv = document.createElement("div")
+        mainDiv.classList.add("card-container")
+        userPicture = document.createElement("img")
+        userPicture.classList.add('round')
+        userName = document.createElement('h3')
+        userLocation = document.createElement('h6')
+        userEmail = document.createElement('p')
+        userPhone = document.createElement('p')
+        userDob = document.createElement('p')
+        userNationality = document.createElement('p')
+        userAddress = document.createElement('p')
+
+        userPicture.src = personObj[i].picture
+        userName.innerHTML = personObj[i].fullname
+        userEmail.innerHTML = personObj[i].email
+        userPhone.innerHTML = `Phone: ${personObj[i].phone}`
+        userDob.innerHTML = `${newParsedDate} | Age: ${personObj[i].age}`
+        userNationality.innerHTML = `Nationality: ${personObj[i].nat}`
+        userAddress.innerHTML = `Location: ${personObj[i].location.city}`
+
+        document.body.append(mainDiv)
+        mainDiv.appendChild(userPicture)
+        mainDiv.appendChild(userName)
+        mainDiv.appendChild(userEmail)
+        mainDiv.appendChild(userPhone)
+        mainDiv.appendChild(userDob)
+        mainDiv.appendChild(userNationality)
+        mainDiv.appendChild(userAddress)
+
+    }
+
+    // for (let i = 0; i < personObj.length; i++) {
+    //     let liItem = document.createElement("li");
+    //     liItem.classList.add("grid-item")
+    //     let userAvatar = document.createElement("img");
+    //     let userFullName = document.createElement("h3")
+    //     let userEmail = document.createElement("p")
+    //     let userPhone = document.createElement("p")
+    //     let userDob = document.createElement("p")
+    //     let userAge = document.createElement("p")
+    //     let userNationality = document.createElement("p")
+    //     let userAddress = document.createElement("p")
+
+    //     // Content
+    //     userAvatar.src = personObj[i].picture
+    //     userFullName.textContent = personObj[i].fullname
+    //     userEmail.textContent = personObj[i].email
+    //     userPhone.textContent = personObj[i].phone
+
+    //     let dateStr = (personObj[i].dob).toString()
+    //     let newDate = new Date(dateStr)
+    //     userDob.textContent = newDate.getDate() + "/" + (newDate.getMonth() + 1) + "/" + (newDate.getFullYear())
+    //     userAge.textContent = personObj[i].age
+    //     userNationality.textContent = personObj[i].nat
+    //     userAddress.textContent = personObj[i].location.street.name
+
+    //     document.body.append(mainUl)
+    //     mainUl.appendChild(liItem)
+    //     liItem.appendChild(userAvatar)
+    //     liItem.appendChild(userFullName)
+    //     liItem.appendChild(userEmail)
+    //     liItem.appendChild(userPhone)
+    //     liItem.appendChild(userDob)
+    //     liItem.appendChild(userAge)
+    //     liItem.appendChild(userNationality)
+    //     liItem.appendChild(userAddress)
+    // }
+}
+
+requestApi()
+
+//Function to 
 async function getUserData() {
     try {
         // userQty = prompt('Users:')
         // const response = await fetch(`https://randomuser.me/api/?results=${userQty}`)
         const response = await fetch('https://randomuser.me/api/?results=5')
-        
+
         const data = await response.json()
 
         data.results.forEach(function (user) {
@@ -62,60 +166,14 @@ async function getUserData() {
 
         //User DOB
         for (let i = 0; i < gridElement.length; i++) {
-            let date = personList[i].dob 
+            let date = personList[i].dob
             let dateStr = date.substr(0, 10)
             gridElement[i].children[4].children[0].textContent = `${dateStr}`
         }
 
-addElement()
+        //addElement()
     } catch (error) {
         console.error(error) // from creation or business logic
     }
 }
-
-getUserData()
-
-
-function addElement() {
-    // create Elements
-    //Main card div
-    const mainUl = document.createElement("ul")
-    mainUl.classList.add("users-grid")
-    const liItem = document.createElement("li");
-    liItem.classList.add("grid-item")
-
-    //Elements within card div
-    const userAvatar = document.createElement("img");
-    const userFullName = document.createElement("h3")
-    const userEmail = document.createElement("p")
-    const userPhone = document.createElement("p")
-    const userDob = document.createElement("p")
-    const userAge = document.createElement("p")
-    const userNationality = document.createElement("p")
-    const userAddress = document.createElement("p")
-    
-    // Content
-    
-    userAvatar.src = '#'
-    userFullName.textContent = "NAME HERE"
-    userEmail.textContent = "email@example.com"
-    userPhone.textContent = '915345321'
-    userDob.textContent = '915345321'
-    userAge.textContent = '100'
-    userNationality.textContent = 'Portuguese'
-    userAddress.textContent = 'Rua das Couves'
-    
-
-    // add all the elements to the document
-    mainUl.append()
-    liItem.appendChild(userAvatar);
-    liItem.appendChild(userFullName)
-    liItem.appendChild(userEmail)
-    liItem.appendChild(userPhone)
-    liItem.appendChild(userAge)
-    liItem.appendChild(userNationality)
-    liItem.appendChild(userAddress)
-  
-    // add the newly created element and its content into the DOM
-    document.body.appendChild(liItem);
-  }
+//getUserData()
