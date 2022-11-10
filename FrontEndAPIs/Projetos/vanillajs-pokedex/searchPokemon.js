b498288950554a03bde8d78cd5054b10645a6c91
João Batista <jb@brightstuff.pt>
2022-11-10 18:01:13 +0000

    new features pokedex vanilla

--- FrontEndAPIs/Projetos/vanillajs-pokedex/searchPokemon.js
+++ FrontEndAPIs/Projetos/vanillajs-pokedex/searchPokemon.js
@@ -1,15 +1,30 @@
 // Fetch APi with the specified pokemon 
-//Pop-up modal with the searched pokemon
 //display the pokemon info
 import { fetchPokemons } from "./fetchApi";
+import { drawPokeElement } from "./drawPoke.js"
 
 const searchPoke = document.querySelector("form")
 
 searchPoke.onsubmit = (evt) => {
-    evt.preventDefault()
-    let value = evt.target[0].value
-    fetchPokemons(value)
-    evt.target.reset()
+    evt.preventDefault() // prevent default behavior
+    let value = evt.target[0].value //store the value from submission
+    console.log(value)
+
+    // Object.keys(sessionStorage).forEach(function(key){
+    //     console.log(key)
+    //  })
+     
+    
+    for (const [key] of Object.entries(sessionStorage)) {
+        //console.log(key)
+        if (key === null) {
+            isNaN(parseInt(value)) ? fetchPokemons(value) : fetchPokemons(parseInt(value)) // parseInt(string) returns NaN if value is string
+        } else
+            return
+
+    }
+    drawPokeElement(JSON.parse(sessionStorage.getItem(`${value}`))) //draws searched element
+    evt.target.reset() //resets form element
 }
 
 export { searchPoke }
